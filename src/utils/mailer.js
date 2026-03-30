b103d2db45077883c18e4febcfacf60f28b0c7c3
MostafaShokry1 <mostafaahmed3281@gmail.com>
2026-03-30 15:12:11 +0200

    last

--- src/utils/mailer.js
+++ src/utils/mailer.js
@@ -3,8 +3,6 @@ import dotenv from "dotenv";
 dotenv.config();
 export const transporter = createTransport({
   service: "gmail",
-  port:587,
-  secure:false,
   auth: {
     user: process.env.EMAIL,
     pass: process.env.EMAIL_PASS,
