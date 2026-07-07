fb8df89127ee3d1ba1a111bac051519039e69fde
MostafaShokry1 <mostafaahmed3281@gmail.com>
2026-07-07 17:50:23 +0300

    ISA

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -48,12 +48,11 @@ const bootstrap = (app) => {
   app.use(
     cors({
       origin: "*",
-      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-      allowedHeaders: ["Content-Type", "Authorization"],
+      allowedHeaders: ["Content-Type", "Authorization", "token"],
+      credentials: true,
     }),
   );
 
-  app.options("*", cors());
   app.use(morgan("dev"));
 
   app.use("/api/v1", v1Router);
