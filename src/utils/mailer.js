d352147ea3c74e21f81be3cb71c0a0b79d4bd8e7
MostafaShokry1 <mostafaahmed3281@gmail.com>
2026-08-11 19:36:48 +0300

    LastEE

--- src/utils/mailer.js
+++ src/utils/mailer.js
@@ -1,17 +1,10 @@
 import { createTransport } from "nodemailer";
 import dotenv from "dotenv";
 dotenv.config();
-
 export const transporter = createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
-  requireTLS: true,
+  service: "gmail",
   auth: {
     user: process.env.EMAIL,
     pass: process.env.EMAIL_PASS,
   },
-  connectionTimeout: 10000,
-  greetingTimeout: 10000,
-  socketTimeout: 10000,
 });
